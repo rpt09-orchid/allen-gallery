e87ae36e188dd6591cb1d48912bcb0702db1ee13
Jonathan Owens <jo@jondionowens.com>
2018-12-20 21:46:13 -0800

    Api (#7)
    * Updated port to render
    
    * Cleaning up working directory
    
    * Updated to ignore an important dir
    
    * Getting ready for elastic
    
    * cleaning
    
    * more cleanup
    
    * Start to refactor codebase for more organization
    
    * Successfully server JSON data from hosted DB
    
    * General clean up
    
    * Updated project name in package.json
    
    * Testing some git config stuff, you can ignore this commit
    
    * Deleted a mock function

--- database/seed.js
+++ database/seed.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
+const db = require('./db')
 const Promise = require('bluebird');
 const readFile = Promise.promisify(require("fs").readFile);
 const utils = require('./dbUtils.js');
 const _ = require('lodash');
 
 const productionBucket = `https://s3.us-east-2.amazonaws.com/elasticbeanstalk-us-east-2-785620446758/property_images/`;
-
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`);
 
 const Schema = mongoose.Schema;
 const PropertySchema = new Schema({
@@ -64,3 +62,5 @@ async function insertProperties() {
 }
 
 insertProperties();
+
+//TEST
