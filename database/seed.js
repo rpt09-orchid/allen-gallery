9e20cc2e4870c6c4428d7044c0a222ffc0c057c7
Allen Hsieh <allenhsiehhr@gmail.com>
2019-01-21 18:38:33 -0800

    Working seed script that i was tinkering with Failure occurs somewhere between 400k and 500k

--- database/seed.js
+++ database/seed.js
@@ -6,32 +6,27 @@ const { photoGroups } = require('./photoGroups');
 mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/galleries'), { useCreateIndex: true, useNewUrlParser: true });
 const db = mongoose.connection;
 
+let arr = [];
+let gallery;
+
 db.on('error', (err) => {
   console.log('error connecting to MongoDB', err);
 })
 db.once('open', () => {
   console.log('mongoose connected');
+  for (let i = 1; i < 400001; i++) {
+    gallery = {
+      id: i,
+      photos: faker.random.arrayElement(photoGroups)
+    }
+    arr.push(gallery);
+  }
+
+  Gallery.insertMany(arr, (error) => {
+    if (error) {
+      console.log('error adding gallery', err);
+    }
+    console.log('length', arr.length);
+    mongoose.disconnect();
+  })
 });
-
-for (let i = 1; i < 10001; i++) {
-
-  let gallery = {
-    id: i,
-    photos: faker.random.arrayElement(photoGroups)
-  }
-
-  if (i === 10000) {
-    Gallery.insertOne(gallery, (err) => {
-      if (err) {
-        console.log('error adding Gallery', err);
-      }
-      mongoose.disconnect();
-    })
-  } else {
-    Gallery.insertOne(gallery, (err) => {
-      if (err) {
-        console.log('error adding Gallery', err);
-      }
-    });
-  }
-}
