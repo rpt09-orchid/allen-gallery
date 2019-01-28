1cba08dd0478613094b9fbda36ff44b7faaf10f2
Allen Hsieh <allenhsiehhr@gmail.com>
2019-01-27 23:45:10 -0800

    refactor per chris and meng notes chris - wanted me to have a dropCollection meng - suggested i use native MongDB driver, MUCH faster

--- database/seed.js
+++ database/seed.js
@@ -4,13 +4,31 @@ const { arrayGenerator } = require('./dataGenerator');
 
 mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/galleries'), { useCreateIndex: true, useNewUrlParser: true });
 let db = mongoose.connection;
+let timerStart;
+let timeElapsed;
+
+try {
+  db.dropCollection('galleries').then( () => {
+    console.log('removed existing galleries table');
+  }).catch(e => {
+    throw e;
+  });
+ } catch (e) {
+  console.log('there was an error');
+ }
 
 let round = 0;
 
 const insertionFactory = () => {
-  return Gallery.GalleryModel.insertMany(arrayGenerator()).catch(error => {
-    console.log('Error inserting records', error);
-    mongoose.disconnect();
+  return new Promise(function(resolve, reject){
+    db.collection('galleries').insertMany(arrayGenerator(), function(error, doc) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
   });
 }
 
@@ -23,15 +41,20 @@ const insertRounds = async () => {
     }
   }
 
+  timeElapsed = (Date.now() - timerStart) / 1000;
+
   mongoose.disconnect(() => {
-    console.log('10m records inserted');
+    console.log(`10m records inserted in ${timeElapsed} seconds`);
   });
 }
 
 db.on('error', (err) => {
   console.log('error connecting', err);
 });
+
 db.once('open', () => {
   console.log('mongoose connected');
+  timerStart = Date.now();
+}).then(() => {
   insertRounds();
 });
