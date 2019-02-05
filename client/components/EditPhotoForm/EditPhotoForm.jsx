dff64e3df8b3c5f116153c3f10bac7d92b3a8405
Allen Hsieh <allenhsiehhr@gmail.com>
2019-02-04 23:04:42 -0800

    posting to photos/:id endpoint

--- client/components/EditPhotoForm/EditPhotoForm.jsx
+++ client/components/EditPhotoForm/EditPhotoForm.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 
-const EditPhotoForm = () => {
-  return (
-    <div>
-      I am the edit photo form
-    </div>
-  )
+class EditPhotoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      url: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console
+    this.setState({
+      url: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    let id = window.location.pathname;
+    let endpoint = `/photos${id}`
+    axios.post(endpoint, {
+      url: this.state.url
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <form onSubmit={ this.handleSubmit }>
+          <label>URL to change main photo to: </label>
+          <input type='url' name='url' value={ this.state.url } onChange={ this.handleChange }/>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+    )
+  }
 }
 
 export default EditPhotoForm;
